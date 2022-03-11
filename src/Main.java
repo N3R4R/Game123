9743560a728276f071b2c9440af3b2753bbb1bbb
N3R4R <altrix544@gmail.com>
2022-03-11 18:28:24 +0300

    ...

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+import Gun.*;
+
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("QQ");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Player player = new Player();
+        int slot;
+        int count;
+
+        while (true) {
+            System.out.format("У игрока %d слотов с оружием,"
+                            + " введите номер, чтобы выстрелить,"
+                            + " -1 чтобы выйти%n",
+                    player.getSlotsCount()
+            );
+
+            count = player.getSlotsCount();
+
+            slot = scanner.nextInt();
+            if (slot < count && slot >= 0){
+                player.shotWithWeapon(slot);
+            }else {
+                System.out.println("В этом слоте нет оружия");
+            }
+            if (slot == -1){
+                break;
+            }
+        }
+        System.out.println("Game over!");
+    }
+}
+
+class Player {
+    private Weapon[] weaponSlots;
+
+    public Player() {
+        weaponSlots = new Weapon[] {
+                // TODO заполнить слоты оружием
+                new Kirka(),
+
+        };
+    }
+
+    public int getSlotsCount() {
+        return weaponSlots.length;
+    }
+
+    public void shotWithWeapon(int slot) {
+        Weapon weapon = weaponSlots[slot];
+        weapon.shot();
     }
 }
